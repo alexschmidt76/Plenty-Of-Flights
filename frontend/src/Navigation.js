0073e6ff7a614fc503a8f3672dc823dab5247274
George Bastawros <george.bastawros@outlook.com>
2023-02-23 15:57:35 -0600

    Added: Home.js and Navigaton.js

---
+++ frontend/src/Navigation.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useContext } from 'react'
+import { useHistory } from "react-router";
+import { CurrentUser } from './contexts/CurrentUser';
+
+function Navigation() {
+
+    const history = useHistory()
+
+    const { currentUser } = useContext(CurrentUser)
+
+    let loginActions = (
+        <>
+            <li>
+                <a href="#" onClick={() => history.push("/sign-up")}>
+                    Sign Up
+                </a>
+            </li>
+            <li>
+                <a href="#" onClick={() => history.push("/login")}>
+                    Login
+                </a>
+            </li>
+        </>
+    )
+
+    if (currentUser) {
+        loginActions = (
+            <li>
+                Logged in as {currentUser.name}
+            </li>
+        )
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#" onClick={() => history.push("/")}>
+                        Home
+                    </a>
+                </li>
+                {loginActions}
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation;
