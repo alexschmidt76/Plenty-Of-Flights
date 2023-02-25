0b8239b3c87586e11dcb67aba202dc9807b8a89e
Alex Schmidt <alex.schmidt767@gmail.com>
2023-02-25 00:28:47 -0600

    trying config connection to db

--- backend/models/index.js
+++ backend/models/index.js
@@ -6,16 +6,17 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')['development'];
 const db = {};
 
-/* if (config.use_env_variable) {
+let sequelize;
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}  */
+} 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+//const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 fs
   .readdirSync(__dirname)
