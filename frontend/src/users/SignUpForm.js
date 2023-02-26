6c0e5830cf958d79ac0e756408a5b36a24c928d0
George Bastawros <george.bastawros@outlook.com>
2023-02-25 18:19:53 -0600

    styling the sign up page

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function SignUpForm() {
 	const navigate = useNavigate()
@@ -23,10 +25,47 @@ function SignUpForm() {
 	}
 
 	return (
-		<>
+		<div style={{ display: 'block', width: 700, padding: 30 }}>
 			<h1>Sign Up</h1>
-			<form onSubmit={handleSubmit}>
-				<div>
+			<Form onSubmit={handleSubmit}>
+			<Form.Group>
+          		<Form.Label>Name:</Form.Label>
+          		<Form.Control
+					required
+					type="text"
+					placeholder="Enter your name" 
+					value={user.name}
+					onChange={e => setUser({ ...user, name: e.target.value })}
+					id="name"
+					name="name"	
+				/>
+        	</Form.Group>
+			<Form.Group>
+          		<Form.Label>Email Address:</Form.Label>
+          		<Form.Control
+					required
+					type="email"
+					placeholder="Enter your email address"
+					value={user.email}
+					onChange={e => setUser({ ...user, email: e.target.value })}
+					id="email"
+					name="email"	
+				/>
+        	</Form.Group>
+			<Form.Group>
+          		<Form.Label>Password:</Form.Label>
+          		<Form.Control
+					required
+					type="password"
+					placeholder="Create your own password"
+					value={user.password}
+					onChange={e => setUser({ ...user, password: e.target.value })}
+					id="password"
+					name="password"	
+				/>
+        	</Form.Group>
+
+				{/* <div>
 					<div>
 						<label htmlFor="name">Create Your Name</label>
 						<input
@@ -61,10 +100,10 @@ function SignUpForm() {
 							name="password"
 						/>
 					</div>
-				</div>
-				<input type="submit" value="Sign Up" />
-			</form>
-		</>
+				</div> */}
+				<Button variant="primary" type="submit">Submit</Button>
+			</Form>
+		</div>
 	)
 }
 
