34592e3bca42226195c2929d5655d1e8510b9f52
Alex Schmidt <alex.schmidt767@gmail.com>
2023-02-26 12:05:55 -0600

    trying url connection to db

--- backend/models/index.js
+++ backend/models/index.js
@@ -9,14 +9,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')['development'];
 const db = {};
 
-let sequelize;
+/* let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-} 
+}  */
 
-//const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 fs
   .readdirSync(__dirname)
