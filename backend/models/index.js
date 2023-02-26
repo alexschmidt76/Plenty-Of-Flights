72a9f5895c3d5bf2eacdd11d99985c2108af01d9
Alex Schmidt <alex.schmidt767@gmail.com>
2023-02-26 14:00:39 -0600

    changed dirname to ./

--- backend/models/index.js
+++ backend/models/index.js
@@ -19,12 +19,12 @@ if (config.use_env_variable) {
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 fs
-  .readdirSync(__dirname)
+  .readdirSync("./") 
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join("./", file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
