6d55452676ec7febc62278e4f5d214716e6faaed
Alex Schmidt <alex.schmidt767@gmail.com>
2023-02-26 14:04:25 -0600

    undid previous and added some logs

--- backend/models/index.js
+++ backend/models/index.js
@@ -19,12 +19,14 @@ if (config.use_env_variable) {
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 fs
-  .readdirSync("./") 
+  .readdirSync(__dirname) 
   .filter(file => {
+    console.log(file, basename)
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = require(path.join("./", file))(sequelize, Sequelize.DataTypes);
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    console.log(model)
     db[model.name] = model;
   });
 
