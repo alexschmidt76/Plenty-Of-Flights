ae9d8527e2dc884697776d4a424e2f45402229fb
George Bastawros <george.bastawros@outlook.com>
2023-02-28 14:05:40 -0600

    Navigation compoment: delete unused codes

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -1,53 +1,36 @@
 import { useContext } from 'react'
-import { useNavigate } from "react-router-dom";
 import { CurrentUser } from './contexts/CurrentUser';
-import {Link} from 'react-router-dom'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
 
 function Navigation() {
-    const navigate = useNavigate()
     const { currentUser } = useContext(CurrentUser)
 
     let loginActions = (
-        <>
-        {/* <Button  variant = 'light' className='navi-btn' ><Link to="/">Home</Link></Button>
-        <Button  variant = 'light' className='navi-btn' ><Link to="/sign-up">Sign-Up</Link></Button>
-        <Button  variant = 'light' className='navi-btn' ><Link to="/login">Login</Link></Button> */}
+      <>
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
-        <Container>
-          <Navbar.Brand href="/"><h1>P O F</h1></Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-          </Nav>
-          {/* <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success"><Link to="/search">Search</Link></Button>
-          </Form> */}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-        </>
+          <Container>
+            <Navbar.Brand href="/"><h1>P O F</h1></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
     )
 
     if (currentUser) {
-        loginActions = (
-            <li>
-                Logged in as {currentUser.name}
-            </li>
-        )
+      loginActions = (
+        <li>
+          <h2>Logged in as {currentUser.name}</h2>
+          </li>
+      )
     }
 
     return (
