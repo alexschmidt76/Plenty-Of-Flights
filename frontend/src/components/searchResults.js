0b4e022645397be86de263171ff0817667c40e4c
George Bastawros <george.bastawros@outlook.com>
2023-02-28 20:02:27 -0600

    added: save flight button-fetch URL to be reviewed

--- frontend/src/components/searchResults.js
+++ frontend/src/components/searchResults.js
@@ -1,14 +1,31 @@
 import React, {Suspense} from 'react'
+import { useNavigate } from "react-router"
+import Button from 'react-bootstrap/Button';
 import Spinner from './spinner';
 // import FlightPath from './flightPath';
 import Map from './Map';
 
 export default function SearchResults () {
 
+  const navigate = useNavigate()
+
+  async function handleSave(e) {
+		e.preventDefault()
+		await fetch(`https://plenty-of-flights-backend.vercel.app/users/:id/flight-paths`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify()
+		})
+		navigate(`/`)
+	}
+
     const renderFlight = () => {
           return(
             <Suspense fallback={<Spinner />}>
-              <Map/>  
+              <Map/>
+              
             </Suspense>
           )
         }
@@ -16,6 +33,9 @@ export default function SearchResults () {
     return(
         <div className="homepage">
             {renderFlight()}
+            <Button variant="primary" type="submit" onClick={handleSave()}>
+                  Save Flight
+              </Button>
         </div>
     )
 }
