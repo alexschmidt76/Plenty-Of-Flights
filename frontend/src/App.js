3fe7b5c4a4492996985c9da43106e3bc8a4c15b1
PRadde <prestonradde@gmail.com>
2023-02-28 18:37:49 -0600

    hope this fixes it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './Map';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Map />
       </header>
     </div>
   );
