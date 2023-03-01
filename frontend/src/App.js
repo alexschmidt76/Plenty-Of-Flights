b1fd0e3af27685bcbe55a9a26d624daeb6677ac0
George Bastawros <george.bastawros@outlook.com>
2023-03-01 00:25:05 -0600

    retrieve.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import React, {useRef, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBar from './components/searchBar';
 import SearchResults from './components/searchResults';
-import Home from './components/Home'
-import Navigation from './components/Navigation'
-import Error404 from './components/Error404'
-import SignUpForm from './components/users/SignUpForm'
-import LoginForm from './components/users/LoginForm'
-import CurrentUserProvider from './context/CurrentUser'
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import Error404 from './components/Error404';
+import SignUpForm from './components/users/SignUpForm';
+import LoginForm from './components/users/LoginForm';
+import MySavedFlights from './components/MySavedFlights';
+import CurrentUserProvider from './context/CurrentUser';
+import { RetrieveContext } from './context/RetreiveContext';
 import { SearchContext } from './context/searchContext';
 import { DataContext } from './context/dataContext';
 import { createResource as fetchSearch} from './airportHelper';
@@ -32,55 +34,49 @@ function App() {
     e.preventDefault()
     setDap(fetchSearch(depart, API_URL))
     setAap(fetchSearch(arrive, API_URL))
-    
-  
-  
-  let searchInput = useRef('')
-
-  const API_URL = 'https://plenty-of-flights-backend.vercel.app/users/:id/flight-paths'
-
-  const handleSearch = (e, term) => {
+  }
+  const handleRetrieve = (e) => {
     e.preventDefault()
-    setData(fetchData(term, API_URL))
-    
-}
-
-const handleRetrieve = (e) => {
-    e.preventDefault()
-    setWishList(fetchRetrieve())
+    setMySavedFlights(fetchRetrieve())
   }
 
   const handleRefresh = () => {
-    setWishList(fetchRetrieve())
+    setMySavedFlights(fetchRetrieve())
   }
-
-}
 
   return (
     <div className="App">
       
       <CurrentUserProvider>
-      <Router>
-        <Navigation />
-        <SearchContext.Provider value={{
-          depart: searchDap,
-          arrive: searchAap,
-          manufacturer: searchMfg,
-          model: searchMdl,
-          handleSearch: handleSearch
-        }}>
-          <SearchBar/>
-          </SearchContext.Provider>
-          <DataContext.Provider value={{dap, aap}}>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/sign-up" element={<SignUpForm/>} />
-          <Route exact path="/login" element={<LoginForm/>} />
-          <Route path="/" element={<Error404/>} />
-          <Route path="/search" element={<SearchResults/>} />
-        </Routes> 
-        </DataContext.Provider>
-      </Router>
+        <Router>
+          <Navigation />
+          <RetrieveContext.Provider value={{
+              data:{wishlist},
+              handleRetrieve:handleRetrieve,
+              handleRefresh:handleRefresh
+            }}>
+
+            <SearchContext.Provider value={{
+              depart: searchDap,
+              arrive: searchAap,
+              manufacturer: searchMfg,
+              model: searchMdl,
+              handleSearch: handleSearch
+            }}>
+            <SearchBar/>
+            </SearchContext.Provider>
+            <DataContext.Provider value={{dap, aap}}>
+              <Routes>
+                <Route exact path="/" element={<Home/>} />
+                <Route exact path="/sign-up" element={<SignUpForm/>} />
+                <Route exact path="/login" element={<LoginForm/>} />
+                <Route path="/" element={<Error404/>} />
+                <Route path="/search" element={<SearchResults/>} />
+                <Route path="/mysavedflights" element={<MySavedFlights/>} />
+              </Routes> 
+            </DataContext.Provider>
+          </RetrieveContext.Provider>
+        </Router>
       </CurrentUserProvider>
     </div>
   );
--- frontend/src/context/RetreiveContext.js
+++ frontend/src/context/RetreiveContext.js
@@ -1,7 +1,7 @@
 import {createContext} from 'react'
 
 export const RetrieveContext = createContext({
-    data: [],
+    data: {},
     handleRetrieve: () => {},
     handleRefresh: () => {}
 })
